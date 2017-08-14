da7c40db52937ee84ffa6f1743c20e0e45d6b824
pavanmangipudi <pavanmangipudi@gmail.com>
2017-08-14 14:11:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,12 @@
 console.log('Loaded!');
 
-//change the main text div
+// change the main text div
 var element= document.getElementById('main-text');
  
  element.innerHTML='New Value';
  
  
-//move the image
+// move the image
 var img=document.getElementById('madi');
 var marginleft=0;
 function moveright() {
