279af181f19dc0042578bd7aad2de475e3fe384d
pavanmangipudi <pavanmangipudi@gmail.com>
2017-08-14 13:27:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,9 @@ var element= document.getElementById('main-text');
  
  element.innerHTML='New Value';
  
+ 
+//move the image
+var img=document.getElementById('img');
+img.onclick=function() {
+    img.style.marginleft='100px';
+};
